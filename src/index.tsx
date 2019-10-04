d2ed9946f9e104db31000cb9103de9a1e4e7dfe6
Emiliano Kira <emiliano.kira@dinoia.com.ar>
2019-10-04 11:47:31 -0300

    theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { createMuiTheme } from "@material-ui/core";
 import {
-	addMonths,
-	isSameDay,
-	isWithinInterval,
-	isAfter,
-	isBefore,
-	isSameMonth,
-	addYears,
-	max,
-	min
+  addMonths,
+  isSameDay,
+  isWithinInterval,
+  isAfter,
+  isBefore,
+  isSameMonth,
+  addYears,
+  max,
+  min
 } from "date-fns";
 import { DateRange, NavigationAction, DefinedRange } from "./types";
 import Menu from "./components/Menu";
@@ -20,170 +20,226 @@ import { parseOptionalDate } from "./utils";
 type Marker = symbol;
 
 export const MARKERS: { [key: string]: Marker } = {
-	FIRST_MONTH: Symbol("firstMonth"),
-	SECOND_MONTH: Symbol("secondMonth")
+  FIRST_MONTH: Symbol("firstMonth"),
+  SECOND_MONTH: Symbol("secondMonth")
 };
 
 const getValidatedMonths = (range: DateRange, minDate: Date, maxDate: Date) => {
-	let { startDate, endDate } = range;
-	if (startDate && endDate) {
-		const newStart = max([startDate, minDate]);
-		const newEnd = min([endDate, maxDate]);
+  let { startDate, endDate } = range;
+  if (startDate && endDate) {
+    const newStart = max([startDate, minDate]);
+    const newEnd = min([endDate, maxDate]);
 
-		return [newStart, isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd];
-	} else {
-		return [startDate, endDate];
-	}
+    return [
+      newStart,
+      isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd
+    ];
+  } else {
+    return [startDate, endDate];
+  }
 };
 
 interface DateRangePickerProps {
-	open: boolean;
-	initialDateRange?: DateRange;
-	definedRanges?: DefinedRange[];
-	minDate?: Date | string;
-	maxDate?: Date | string;
-	onChange: (dateRange: DateRange) => void;
+  open: boolean;
+  initialDateRange?: DateRange;
+  definedRanges?: DefinedRange[];
+  minDate?: Date | string;
+  maxDate?: Date | string;
+  onChange: (dateRange: DateRange) => void;
 }
 
 const theme = createMuiTheme({
-	palette: {
-		primary: {
-		  light: 'rgba(255,0,0)',
-		  main: 'rgba(0,255,0)',
-		  dark: 'rgba(0,0,255)',
-		  contrastText: '#fff',
-		},
-		secondary: {
-			light: 'rgba(255,0,0)',
-			main: 'rgba(0,255,0)',
-			dark: 'rgba(0,0,255)',
-		  contrastText: '#000',
-		},
-	}
+  palette: {
+    common: { black: "#000", white: "#fff" },
+    type: "dark",
+    primary: {
+      main: "#90caf9",
+      light: "rgb(166, 212, 250)",
+      dark: "rgb(100, 141, 174)",
+      contrastText: "rgba(0, 0, 0, 0.87)"
+    },
+    secondary: {
+      main: "#f48fb1",
+      light: "rgb(246, 165, 192)",
+      dark: "rgb(170, 100, 123)",
+      contrastText: "rgba(0, 0, 0, 0.87)"
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#fff"
+    },
+    grey: {
+      "50": "#fafafa",
+      "100": "#f5f5f5",
+      "200": "#eeeeee",
+      "300": "#e0e0e0",
+      "400": "#bdbdbd",
+      "500": "#9e9e9e",
+      "600": "#757575",
+      "700": "#616161",
+      "800": "#424242",
+      "900": "#212121",
+      A100: "#d5d5d5",
+      A200: "#aaaaaa",
+      A400: "#303030",
+      A700: "#616161"
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+    text: {
+      primary: "#fff",
+      secondary: "rgba(255, 255, 255, 0.7)",
+      disabled: "rgba(255, 255, 255, 0.5)",
+      hint: "rgba(255, 255, 255, 0.5)"
+    },
+    divider: "rgba(255, 255, 255, 0.12)",
+    background: {
+      paper: "#424242",
+      default: "#121212"
+    },
+    action: {
+      active: "#fff",
+      hover: "rgba(255, 255, 255, 0.1)",
+      hoverOpacity: 0.1,
+      selected: "rgba(255, 255, 255, 0.2)",
+      disabled: "rgba(255, 255, 255, 0.3)",
+      disabledBackground: "rgba(255, 255, 255, 0.12)"
+    }
+  }
 });
 
-const DateRangePickerImpl: React.FunctionComponent<DateRangePickerProps> = props => {
-	const today = new Date();
+const DateRangePickerImpl: React.FunctionComponent<
+  DateRangePickerProps
+> = props => {
+  const today = new Date();
 
-	const {
-		open,
-		onChange,
-		initialDateRange,
-		minDate,
-		maxDate,
-		definedRanges = defaultRanges
-	} = props;
+  const {
+    open,
+    onChange,
+    initialDateRange,
+    minDate,
+    maxDate,
+    definedRanges = defaultRanges
+  } = props;
 
-	const minDateValid = parseOptionalDate(minDate, addYears(today, -10));
-	const maxDateValid = parseOptionalDate(maxDate, addYears(today, 10));
-	const [intialFirstMonth, initialSecondMonth] = getValidatedMonths(
-		initialDateRange || {},
-		minDateValid,
-		maxDateValid
-	);
+  const minDateValid = parseOptionalDate(minDate, addYears(today, -10));
+  const maxDateValid = parseOptionalDate(maxDate, addYears(today, 10));
+  const [intialFirstMonth, initialSecondMonth] = getValidatedMonths(
+    initialDateRange || {},
+    minDateValid,
+    maxDateValid
+  );
 
-	// console.log("rendering DateRangePicker");
-	const [dateRange, setDateRange] = React.useState<DateRange>({ ...initialDateRange });
-	const [hoverDay, setHoverDay] = React.useState<Date>();
-	const [firstMonth, setFirstMonth] = React.useState<Date>(intialFirstMonth || today);
-	const [secondMonth, setSecondMonth] = React.useState<Date>(
-		initialSecondMonth || addMonths(firstMonth, 1)
-	);
+  // console.log("rendering DateRangePicker");
+  const [dateRange, setDateRange] = React.useState<DateRange>({
+    ...initialDateRange
+  });
+  const [hoverDay, setHoverDay] = React.useState<Date>();
+  const [firstMonth, setFirstMonth] = React.useState<Date>(
+    intialFirstMonth || today
+  );
+  const [secondMonth, setSecondMonth] = React.useState<Date>(
+    initialSecondMonth || addMonths(firstMonth, 1)
+  );
 
-	const { startDate, endDate } = dateRange;
+  const { startDate, endDate } = dateRange;
 
-	// handlers
-	const setFirstMonthValidated = (date: Date) => {
-		if (isBefore(date, secondMonth)) {
-			setFirstMonth(date);
-		}
-	};
+  // handlers
+  const setFirstMonthValidated = (date: Date) => {
+    if (isBefore(date, secondMonth)) {
+      setFirstMonth(date);
+    }
+  };
 
-	const setSecondMonthValidated = (date: Date) => {
-		if (isAfter(date, firstMonth)) {
-			setSecondMonth(date);
-		}
-	};
+  const setSecondMonthValidated = (date: Date) => {
+    if (isAfter(date, firstMonth)) {
+      setSecondMonth(date);
+    }
+  };
 
-	const setDateRangeValidated = (range: DateRange) => {
-		let { startDate: newStart, endDate: newEnd } = range;
-		if (newStart && newEnd) {
-			range.startDate = newStart = max([newStart, minDateValid]);
-			range.endDate = newEnd = min([newEnd, maxDateValid]);
-			setDateRange(range);
-			onChange(range);
-			setFirstMonth(newStart);
-			setSecondMonth(isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd);
-		}
-	};
+  const setDateRangeValidated = (range: DateRange) => {
+    let { startDate: newStart, endDate: newEnd } = range;
+    if (newStart && newEnd) {
+      range.startDate = newStart = max([newStart, minDateValid]);
+      range.endDate = newEnd = min([newEnd, maxDateValid]);
+      setDateRange(range);
+      onChange(range);
+      setFirstMonth(newStart);
+      setSecondMonth(
+        isSameMonth(newStart, newEnd) ? addMonths(newStart, 1) : newEnd
+      );
+    }
+  };
 
-	const onDayClick = (day: Date) => {
-		if (startDate && !endDate && !isBefore(day, startDate)) {
-			const newRange = { startDate, endDate: day };
-			onChange(newRange);
-			setDateRange(newRange);
-		} else {
-			setDateRange({ startDate: day, endDate: undefined });
-		}
-		setHoverDay(day);
-	};
+  const onDayClick = (day: Date) => {
+    if (startDate && !endDate && !isBefore(day, startDate)) {
+      const newRange = { startDate, endDate: day };
+      onChange(newRange);
+      setDateRange(newRange);
+    } else {
+      setDateRange({ startDate: day, endDate: undefined });
+    }
+    setHoverDay(day);
+  };
 
-	const onMonthNavigate = (marker: Marker, action: NavigationAction) => {
-		if (marker == MARKERS.FIRST_MONTH) {
-			const firstNew = addMonths(firstMonth, action);
-			if (isBefore(firstNew, secondMonth)) setFirstMonth(firstNew);
-		} else {
-			const secondNew = addMonths(secondMonth, action);
-			if (isBefore(firstMonth, secondNew)) setSecondMonth(secondNew);
-		}
-	};
+  const onMonthNavigate = (marker: Marker, action: NavigationAction) => {
+    if (marker == MARKERS.FIRST_MONTH) {
+      const firstNew = addMonths(firstMonth, action);
+      if (isBefore(firstNew, secondMonth)) setFirstMonth(firstNew);
+    } else {
+      const secondNew = addMonths(secondMonth, action);
+      if (isBefore(firstMonth, secondNew)) setSecondMonth(secondNew);
+    }
+  };
 
-	const onDayHover = (date: Date) => {
-		if (startDate && !endDate) {
-			if (!hoverDay || !isSameDay(date, hoverDay)) {
-				setHoverDay(date);
-			}
-		}
-	};
+  const onDayHover = (date: Date) => {
+    if (startDate && !endDate) {
+      if (!hoverDay || !isSameDay(date, hoverDay)) {
+        setHoverDay(date);
+      }
+    }
+  };
 
-	// helpers
-	const inHoverRange = (day: Date) => {
-		return (startDate &&
-			!endDate &&
-			hoverDay &&
-			isAfter(hoverDay, startDate) &&
-			isWithinInterval(day, {start: startDate, end: hoverDay})) as boolean;
-	};
+  // helpers
+  const inHoverRange = (day: Date) => {
+    return (startDate &&
+      !endDate &&
+      hoverDay &&
+      isAfter(hoverDay, startDate) &&
+      isWithinInterval(day, { start: startDate, end: hoverDay })) as boolean;
+  };
 
-	const helpers = {
-		inHoverRange
-	};
+  const helpers = {
+    inHoverRange
+  };
 
-	const handlers = {
-		onDayClick,
-		onDayHover,
-		onMonthNavigate
-	};
+  const handlers = {
+    onDayClick,
+    onDayHover,
+    onMonthNavigate
+  };
 
-	return <MuiThemeProvider theme={theme}>
-		{
-			open ? (
-				<Menu
-					dateRange={dateRange}
-					minDate={minDateValid}
-					maxDate={maxDateValid}
-					ranges={definedRanges}
-					firstMonth={firstMonth}
-					secondMonth={secondMonth}
-					setFirstMonth={setFirstMonthValidated}
-					setSecondMonth={setSecondMonthValidated}
-					setDateRange={setDateRangeValidated}
-					helpers={helpers}
-					handlers={handlers}
-				/>
-			) : null
-		}
-	</MuiThemeProvider>
+  return (
+    <MuiThemeProvider theme={theme}>
+      {open ? (
+        <Menu
+          dateRange={dateRange}
+          minDate={minDateValid}
+          maxDate={maxDateValid}
+          ranges={definedRanges}
+          firstMonth={firstMonth}
+          secondMonth={secondMonth}
+          setFirstMonth={setFirstMonthValidated}
+          setSecondMonth={setSecondMonthValidated}
+          setDateRange={setDateRangeValidated}
+          helpers={helpers}
+          handlers={handlers}
+        />
+      ) : null}
+    </MuiThemeProvider>
+  );
 };
 
 export { DateRange, DefinedRange } from "./types";
